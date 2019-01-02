c048867a744d3971dab5abbfee9cd917d17660a3
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-01-02 14:30:47 +0100

    project with nodejs and passport

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const url = 'mongodb+srv://admin:1442@projectnode-kgrep.mongodb.net/test?retryWrites=true'; 
+
+module.exports = {
+  MongoURI : 'mongodb+srv://admin:1442@projectnode-kgrep.mongodb.net/test?retryWrites=true'
+}; 
